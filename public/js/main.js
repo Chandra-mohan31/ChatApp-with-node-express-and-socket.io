e33fce3922b052685d670541c6a4538f38dec3d9
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2021-03-22 20:47:57 +0530

    complete working chat app

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,153 @@
+// const chatForm = document.getElementById('chat-form');
+// const socket = io();
+// const chatMessages = document.querySelector('.chat-messages');
+
+// // get username and room from url
+
+// const {username,room} = Qs.parse(location.search,{
+//     ignoreQueryPrefix: true
+// })
+
+// console.log(username,room);
+
+
+
+// socket.emit('joinroom',{username,room});
+
+// socket.on('roomUsers', ({ room, users }) => {
+//     outputRoomName(room);
+//     outputUsers(users);
+//   });
+  
+
+// //message from the server
+// socket.on('message', (message) => {
+//     console.log(message);
+//     outputMessage(message);
+    
+//     chatMessages.scrollTop= chatMessages.scrollHeight;
+
+//     //scroll down
+    
+//   });
+
+// chatForm.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+//     //get msg text
+
+//     const msg = e.target.elements.msg.value;
+//     console.log(msg);
+//     socket.emit('chatMessage',msg)
+//     //clear input
+//     e.target.elements.msg.value = '';
+//     e.target.elements.msg.focus();
+// })
+
+// //output the message
+// function outputMessage(message) {
+//     const div = document.createElement('div');
+//     div.classList.add('message');
+//     const p = document.createElement('p');
+//     p.classList.add('meta');
+//     p.innerText = message.username;
+//     p.innerHTML += `<span>${message.time}</span>`;
+//     div.appendChild(p);
+//     const para = document.createElement('p');
+//     para.classList.add('text');
+//     para.innerText = message.text;
+//     div.appendChild(para);
+//     document.querySelector('.chat-messages').appendChild(div);
+//   }
+
+
 const chatForm = document.getElementById('chat-form');
-const socket = io();
 const chatMessages = document.querySelector('.chat-messages');
-//message from the server
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message from server
 socket.on('message', (message) => {
-    console.log(message);
-    outputMsg(message);
-    
-    chatMessages.scrollTop= chatMessages.scrollHeight;
+  console.log(message);
+  outputMessage(message);
 
-    //scroll down
-    
-  });
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
-chatForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    //get msg text
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    const msg = e.target.elements.msg.value;
-    console.log(msg);
-    socket.emit('chatMessage',msg)
-    //clear input
-    e.target.elements.msg.value = '';
-    e.target.elements.msg.focus();
-})
+  // Get message text
+  let msg = e.target.elements.msg.value;
 
-//output the message
-const outputMsg = (message) => {
-    const div = document.createElement('div');
-    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
-    <p class="text">
-        ${message.text}
-    </p>`
+  msg = msg.trim();
 
-    document.querySelector('.chat-messages').appendChild(div)
+  if (!msg) {
+    return false;
+  }
 
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
 }
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
+
